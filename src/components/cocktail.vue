41d576802ff4d067c0bf506594779c9c673a4349
HEIM Yannick <yannick.heim4@etu.univ-lorraine.fr>
2023-10-19 11:46:46 +0200

    fichier cocktail.vue

---
+++ src/components/cocktail.vue
@@ -0,0 +1,25 @@
+<script>
+   export default [
+  {
+    nom: "Bloody Tonic",
+    alcool: ["Vodka", "Tonic"],
+    recette: ["4cl de vodka Poliakov","5cl de tonic La French SVP","4cl de jus de cranberry"],
+    img: "https://www.destinationcocktails.fr/wp-content/uploads/2023/10/recette-cocktail-bloody-tonic-1.jpg",
+    show: false,
+  },
+  {
+    nom: "Sea Breeze",
+    alcool: ["Vodka"],
+    recette: ["4cl de vodka Poliakov","12cl de jus de cranberry","3cl de jus de pamplemousse"],
+    img: "https://www.destinationcocktails.fr/wp-content/uploads/2023/08/recette-cocktail-sea-breeze.jpg",
+    show: false,
+  },
+  {
+    nom: "Tequila Paf",
+    alcool: ["Tequila"],
+    recette: ["3cl de Tequila San José","6cl de Tonic la French SVP","1 quartier de citron vert","sel"],
+    img: "https://www.destinationcocktails.fr/wp-content/uploads/2022/05/cocktail-tequila-paf.jpg",
+    show: false,
+  },
+];
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,9 @@
 <script>
+import cocktails from "../components/cocktail.vue";
 export default {
   data: () => ({
     icons: ["mdi-rewind", "mdi-play", "mdi-fast-forward"], 
-    cocktail: [
-      {
-        nom: "Bloody Tonic",
-        alcool: ["Vodka", "Tonic"],
-        recette: ["4cl de vodka Poliakov","5cl de tonic La French SVP","4cl de jus de cranberry"],
-        img: "https://www.destinationcocktails.fr/wp-content/uploads/2023/10/recette-cocktail-bloody-tonic-1.jpg",
-        show: false,
-      },
-      {
-        nom: "Sea Breeze",
-        alcool: ["Vodka"],
-        recette: ["4cl de vodka Poliakov","12cl de jus de cranberry","3cl de jus de pamplemousse"],
-        img: "https://www.destinationcocktails.fr/wp-content/uploads/2023/08/recette-cocktail-sea-breeze.jpg",
-        show: false,
-      },
-      {
-        nom: "Tequila Paf",
-        alcool: ["Tequila"],
-        recette: ["3cl de Tequila San José","6cl de Tonic la French SVP","1 quartier de citron vert","sel"],
-        img: "https://www.destinationcocktails.fr/wp-content/uploads/2022/05/cocktail-tequila-paf.jpg",
-        show: false,
-      },
-    ],
+    cocktail: cocktails,
     transparent: "rgba(255, 255, 255, 0)",
   }),
 };
